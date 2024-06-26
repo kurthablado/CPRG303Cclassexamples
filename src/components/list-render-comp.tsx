6cbde375f035054c87181c74562690f515c7fed7
Derek Yadlowski <Derek.Yadlowski@sait.ca>
2024-06-26 09:39:43 -0600

    update for Typescript

--- src/components/list-render-comp.tsx
+++ src/components/list-render-comp.tsx
@@ -1,7 +1,8 @@
+import { ReactNode } from "react";
 import { Text } from "react-native";
 
 
-export default function ListItemRenderComp({item}){
+export default function ListItemRenderComp({item = {id:0, text:"default text"}}){
     
     return(
         <Text>{item.text}</Text>
